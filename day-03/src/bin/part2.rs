0c58b75ba5944e66697440759dcca81cef7cdd7c
Olivier Leduc <leduc.o@gmail.com>
2023-12-03 19:57:01 -0500

    day-03 part 1

---
+++ day-03/src/bin/part2.rs
@@ -0,0 +1,10 @@
+use anyhow::{Context, Result};
+
+use day_03::part2::process;
+
+fn main() -> Result<()> {
+    let input = include_str!("../../input2.txt");
+    let output = process(input).context("process part 2")?;
+    println!("{output}");
+    Ok(())
+}
