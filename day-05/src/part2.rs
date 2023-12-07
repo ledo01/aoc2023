837036350d5bac48b74b87e776055fe2cdcceb64
Olivier Leduc <leduc.o@gmail.com>
2023-12-07 08:24:56 -0500

    day-05 part 2 -- with rayon

--- day-05/src/part2.rs
+++ day-05/src/part2.rs
@@ -1,7 +1,34 @@
+use std::ops::Range;
+
 use anyhow::Result;
+use itertools::Itertools;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+use crate::map::Map;
 
 pub fn process(input: &str) -> Result<String> {
-    todo!()
+    let (seeds, maps) = input.split_once("\n\n").unwrap();
+    let seeds: Vec<Range<u64>> = seeds
+        .strip_prefix("seeds: ")
+        .expect("seeds to be valid")
+        .split_whitespace()
+        .map(|s| s.parse::<u64>().expect("seed to be a number"))
+        .tuples()
+        .map(|(seed, range)| seed..seed + range)
+        .collect();
+
+    let maps: Vec<Map> = maps
+        .split("\n\n")
+        .map(|map| map.parse().expect("to ve a valid map"))
+        .collect();
+
+    Ok(seeds
+        .into_par_iter()
+        .flat_map(|r| r.clone())
+        .map(|seed| maps.iter().fold(seed, |source, map| map.get(source)))
+        .min()
+        .expect("to have a min")
+        .to_string())
 }
 
 #[cfg(test)]
@@ -9,7 +36,52 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse_seed_to_soil() {
+        let input = r"seed-to-soil map:
+50 98 2
+52 50 48";
+        assert_eq!(
+            input.parse::<Map>().unwrap(),
+            Map(vec![(98..100, 50..52), (50..98, 52..100)])
+        );
+    }
+
+    #[test]
     fn test_process() -> Result<()> {
-        todo!()
+        let input = r"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4";
+        assert_eq!(process(input)?, "46");
+        Ok(())
     }
 }
