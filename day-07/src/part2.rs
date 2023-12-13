7223f16301183c10d69cb27ece89ba8ce83c7519
Olivier Leduc <leduc.o@gmail.com>
2023-12-12 21:41:02 -0500

    day-07 part 1,2

---
+++ day-07/src/part2.rs
@@ -0,0 +1,181 @@
+use std::cmp::Ordering;
+
+use anyhow::Result;
+use itertools::Itertools;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfKind,
+    FullHouse,
+    FourOfKind,
+    FiveOfKind,
+}
+
+impl HandType {
+    fn upgrade(self) -> Self {
+        match self {
+            HandType::HighCard => HandType::OnePair,
+            HandType::OnePair => HandType::ThreeOfKind,
+            HandType::TwoPair => HandType::FullHouse,
+            HandType::ThreeOfKind => HandType::FourOfKind,
+            HandType::FullHouse => HandType::FourOfKind,
+            HandType::FourOfKind => HandType::FiveOfKind,
+            HandType::FiveOfKind => HandType::FiveOfKind,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Card {
+    J,
+    N(u32),
+    T,
+    Q,
+    K,
+    A,
+}
+
+impl TryFrom<char> for Card {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'A' => Ok(Card::A),
+            'K' => Ok(Card::K),
+            'Q' => Ok(Card::Q),
+            'J' => Ok(Card::J),
+            'T' => Ok(Card::T),
+            c => c.to_digit(10).ok_or(()).map(Card::N),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Hand {
+    hand_type: HandType,
+    cards: [Card; 5],
+}
+
+impl Hand {
+    pub fn new(value: &str) -> Hand {
+        let cards: [Card; 5] = value
+            .chars()
+            .flat_map(|c| c.try_into())
+            .collect::<Vec<_>>()
+            .try_into()
+            .expect("to only have 5 card");
+        let counts = cards.iter().counts();
+        let jokers = *counts.get(&Card::J).unwrap_or(&0);
+        let counts: Vec<_> = counts
+            .iter()
+            .filter(|hand| **hand.0 != Card::J)
+            .sorted_by(|a, b| b.1.cmp(a.1))
+            .collect();
+        let mut hand_type = match counts.len() + jokers {
+            1 => HandType::FiveOfKind,
+            2 if *counts[0].1 == 4 => HandType::FourOfKind,
+            2 if *counts[0].1 == 3 => HandType::FullHouse,
+            3 if *counts[0].1 == 3 => HandType::ThreeOfKind,
+            3 if *counts[0].1 == 2 => HandType::TwoPair,
+            4 => HandType::OnePair,
+            _ => HandType::HighCard,
+        };
+
+        for _ in 0..jokers {
+            hand_type = hand_type.upgrade()
+        }
+
+        Hand { cards, hand_type }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.hand_type.cmp(&other.hand_type) {
+            Ordering::Equal => {
+                for i in 0..5 {
+                    match self.cards[i].cmp(&other.cards[i]) {
+                        Ordering::Equal => {}
+                        ord => return ord,
+                    }
+                }
+            }
+            ord => return ord,
+        }
+        Ordering::Equal
+    }
+}
+
+pub fn process(input: &str) -> Result<String> {
+    let hands: u32 = input
+        .lines()
+        .flat_map(|line| line.split_once(' '))
+        .map(|(hand, c)| (Hand::new(hand), c.parse::<u32>().unwrap()))
+        .sorted_by(|a, b| a.0.cmp(&b.0))
+        .enumerate()
+        .map(|(idx, (_, val))| (idx as u32 + 1) * val)
+        .sum();
+
+    Ok(hands.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case('A', Card::A)]
+    #[case('K', Card::K)]
+    #[case('3', Card::N(3))]
+    fn test_parse_card(#[case] value: char, #[case] expected: Card) -> Result<()> {
+        let card: Card = value.try_into().unwrap();
+        assert_eq!(card, expected);
+        Ok(())
+    }
+
+    #[rstest]
+    #[case("23456", HandType::HighCard)]
+    #[case("A23A4", HandType::OnePair)]
+    #[case("23432", HandType::TwoPair)]
+    #[case("TTT98", HandType::ThreeOfKind)]
+    #[case("23332", HandType::FullHouse)]
+    #[case("AA8AA", HandType::FourOfKind)]
+    #[case("AAAAA", HandType::FiveOfKind)]
+    #[case("AAAAJ", HandType::FiveOfKind)]
+    #[case("JAAJJ", HandType::FiveOfKind)]
+    #[case("KTJJT", HandType::FourOfKind)]
+    fn test_new_card(#[case] cards: &str, #[case] expected: HandType) {
+        let card = Hand::new(cards);
+        assert_eq!(card.hand_type, expected);
+    }
+
+    #[test]
+    fn test_ord() {
+        assert!(Hand::new("33332") > Hand::new("2AAAA"));
+        assert!(Hand::new("33332") > Hand::new("23456"));
+        assert!(Hand::new("33334") > Hand::new("33332"));
+        assert!(Hand::new("3333A") > Hand::new("33332"));
+        assert!(Hand::new("QQQJA") > Hand::new("T55J5"));
+    }
+
+    #[test]
+    fn test_process() -> Result<()> {
+        let input = r"32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
+        assert_eq!(process(input)?, "5905");
+        Ok(())
+    }
+}
