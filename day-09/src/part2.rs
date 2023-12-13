63a7349a285d29d6f67cae4310ff24a079ce54c1
Olivier Leduc <leduc.o@gmail.com>
2023-12-13 00:22:43 -0500

    day-09 part 2

--- day-09/src/part2.rs
+++ day-09/src/part2.rs
@@ -1,7 +1,30 @@
 use anyhow::Result;
 
+fn predict_prev(values: &[i64]) -> i64 {
+    if values.iter().all(|v| *v == 0) {
+        return 0;
+    }
+    let deltas: Vec<_> = values
+        .windows(2)
+        .rev()
+        .map(|window| window[1] - window[0])
+        .rev()
+        .collect();
+    let first = values[0];
+    first - predict_prev(&deltas)
+}
+
 pub fn process(input: &str) -> Result<String> {
-    todo!()
+    let results = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .filter_map(|n| n.parse().ok())
+                .collect()
+        })
+        .map(|values: Vec<_>| predict_prev(&values))
+        .sum::<i64>();
+    Ok(results.to_string())
 }
 
 #[cfg(test)]
@@ -9,7 +32,18 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_predict_next() {
+        assert_eq!(predict_prev(&[0, 3, 6, 9, 12, 15]), -3);
+        assert_eq!(predict_prev(&[1, 3, 6, 10, 15, 21]), 0);
+        assert_eq!(predict_prev(&[10, 13, 16, 21, 30, 45]), 5);
+    }
+
+    #[test]
     fn test_process() -> Result<()> {
-        todo!()
+        let input = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+        assert_eq!(process(input)?, "2");
+        Ok(())
     }
 }
